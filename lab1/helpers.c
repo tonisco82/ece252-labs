bff9840df03aeb4387789bd9c56e26f7a6d99e51
Devon Miller-Junk <dsmiller@uwaterloo.ca>
2020-05-22 17:47:49 -0400

    Add helper file descriptions

--- lab1/helpers.c
+++ lab1/helpers.c
@@ -6,9 +6,12 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+
 // What the 8-Byte Header of the PNG should look like in integer format
 int png_header[8] = {-119, 80, 78, 71, 13, 10, 26, 10};
 
+// Returns 1 if the file is a png, 0 if not, -1 on error
+// file_path is the relative file path to the file in question
 int is_png(char* file_path){
 
     int buffer_len = 8;
@@ -41,5 +44,10 @@ int is_png(char* file_path){
     return 1;
 }
 
-//get_data_IHDR(U8 *dest, U64 *dest_len, U8 *source,  U64 source_len);
+// Place the IHDR information into the destination location.
+// Destination should have 13 bytes of space available.
+// For information about how the IHDR data is formatted see the Readme
+int get_data_IHDR(char* file_path, char*destination);
+
+
 
