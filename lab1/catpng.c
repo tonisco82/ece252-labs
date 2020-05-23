c108678e2cffb205aac1b2f6ef6146c1587d6842
Braden Bakker <bgobakke@uwaterloo.ca>
2020-05-23 18:09:57 -0400

    untested full feature catpng

--- lab1/catpng.c
+++ lab1/catpng.c
@@ -19,6 +19,16 @@
 #include "lab_png.h"
 #include "zutil.h"
 
+int crcgen (struct chunk* data){
+	U32 crcflag = 0;
+    ntohl(data->p_data);
+	U32 testcrc = crc(data->p_data, data->length)
+	if (testcrc != data->crc){
+		crcflag = testcrc;
+	}
+	return crcflag;  //returns 0 if pass, returns crc found if fail
+}
+
 int main(int argc, char *argv[]) {
 
     if(argc <= 2) {
@@ -35,20 +45,86 @@ int main(int argc, char *argv[]) {
    		get_png(fp, imgs[i]); 
    	}
 
-   	int height;
+   	int height = 0;
+   	int width = 0;
+   	data_IHDR_p calcs[argc-1];
 
    	for (int i=0; i<(argc-1); i++){  //loop get IHDR data for dimensions
-   		//get_IHDR(imgs[i]->p_data);
+   		get_data_IHDR(imgs[i]->p_IHDR, calcs[i]);
+   		height += calcs[i]->height;
    	}
+   	width = calcs[0]->width;
 
-   	
+	void* catbuf = malloc(height*((width*4)+1));
+	U64 len_def = 0;      /* compressed data length                        */
+    U64 len_inf = 0;      /* uncompressed data length                      */
+    U64 len_tot = 0;      //running tally of total length
+    int ret = 0;        //debug param
+
    	for (int i=0; i<(argc-1); i++){ //loop inflate data
-   		void*a = malloc()
-   		if (0 != mem_inf()
+   		len_def = imgs[i]->p_IDAT->length;
+   		len_inf = 0;
+   		ret = mem_inf(catbuf+len_tot, &len_inf, imgs[i]->IDAT->p_data, len_def); //automatically concatenate inflated data to buffer
+	    if (ret !=0){
+	        fprintf(stderr,"mem_def failed. ret = %d.\n", ret);
+	        return ret;
+	    }
+	    len_tot += len_inf;  //keep running total for buffer offset on next data inflate
+    }
+
+    void* newdata = malloc(height*((width*4)+1)); //new data buffer
+
+    //deflate data to new data buffer
+    ret = mem_def(newdata &len_def, catbuf, (height*((width*4)+1)), Z_DEFAULT_COMPRESSION);
+    if (ret !=0){
+        fprintf(stderr,"mem_def failed. ret = %d.\n", ret);
+        return ret;
+    }
+    // now we should have the proper compressed IDAT data
+    //from now on use initial png to create new one
+    U32 newheight = height;
+
+    newpng = imgs[0];
+    newpng->IHDR->p_data+sizeof(U32) = htonl(newheight); //directly replace height in data
+    newpng->IDAT->p_data = newdata;
+    newpng->IDAT->length = len_def;
+    newpng->IDAT->crc = crcgen(newpng->IDAT);
+    newpng->IHDR->crc = crcgen(newpng->IHDR);
+
+    //we should now have newpng with all of the data we need
+    //now adjust data for easy writing
+    htonl(newpng->IDAT->length);
+    htonl(newpng->IDAT->crc);
+    htonl(newpng->IHDR->length);
+    htonl(newpng->IHDR->crc);
+
+   	FILE *fpw;
+	fpw = fopen ("all.png", "wb+");
+	//write png header
+	int png_header[8] = {-119, 80, 78, 71, 13, 10, 26, 10};
+	fwrite(&png_header[0], sizeof(png_header), 1, fpw);
+	//write IHDR
+	fwrite(&(newpng->IHDR->length), sizeof(newpng->IHDR->length), 1, fpw); 
+	fwrite(&(newpng->IHDR->type[0]), sizeof(newpng->IHDR->type), 1, fpw);
+	fwrite(newpng->IHDR->p_data, newpng->IHDR->length, 1, fpw);
+	fwrite(&(newpng->IHDR->crc), sizeof(newpng->IHDR->crc), 1, fpw);
+	//write IDAT
+	fwrite(&(newpng->IDAT->length), sizeof(newpng->IDAT->length), 1, fpw); 
+	fwrite(&(newpng->IDAT->type[0]), sizeof(newpng->IDAT->type), 1, fpw);
+	fwrite(newpng->IDAT->p_data, newpng->IDAT->length, 1, fpw);
+	fwrite(&(newpng->IDAT->crc), sizeof(newpng->IDAT->crc), 1, fpw);
+	//write IEND
+	fwrite(&(newpng->IEND->length), sizeof(newpng->IEND->length), 1, fpw); 
+	fwrite(&(newpng->IEND->type[0]), sizeof(newpng->IEND->type), 1, fpw);
+	fwrite(newpng->IEND->p_data, newpng->END->length, 1, fpw);
+	fwrite(&(newpng->IEND->crc), sizeof(newpng->IEND->crc), 1, fpw);
+
+	for (int i=0; i<(argc-1); i++){  //loop to free memory
+   		free(calcs[i]);
+   		free(imgs[i]->IHDR->p_data);
+   		free(imgs[i]->IDAT->p_data);
+   		free(imgs[i]->IEND->p_data);
+   		free(imgs[i]);
    	}
-   	//concatenate and deflate data
-
-   	//write to new file
-
    	return 0;
 }
